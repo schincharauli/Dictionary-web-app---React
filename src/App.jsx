f21d632d5bdf67b74ed009d584f9532b468b398a
pc <pc@pcs-MacBook-Pro.local>
2023-05-23 14:09:06 +0400

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import logo from "../public/images/logo.svg";
+import moonIcon from "../public/images/icon-moon.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+
+    if (newTheme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className={`h-screen ${theme === "dark" ? "bg-dark" : ""}`}>
+      <nav>
+        <span lassName="pl-3">
+          <img src={logo} alt="" />
+        </span>
+
+        <button>
+          <label className="relative inline-flex items-center cursor-pointer">
+            <input type="checkbox" className="sr-only peer" />
+            <div
+              onClick={changeTheme}
+              className={`w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 dark:peer-focus:ring-violet rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-violet`}
+            ></div>
+            <span>
+              <img
+                src={moonIcon}
+                alt=""
+                className={"dark:path-stroke-violet"}
+              />
+            </span>
+          </label>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      </nav>
+
+      <section>
+        <p className={`dark:text-white font-Inter`}>
+          This text will be white in dark mode.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <p className="dark:text-white font-Lora">
+          This text will be white in dark mode.
+        </p>
+        <p className="dark:text-white font-Inconsolata">
+          This text will be white in dark mode.
+        </p>
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColorDark: "#050505",
+        bgColorLight: "#050505",
+        inputColorDark: "#1F1F1F",
+        textColor: "#2D2D2D",
+        greyTextLightMode: "#757575",
+        inputColorLight: "#F4F4F4",
+        violet: "#A445ED",
+        red: "#FF5252",
+        dark: "#050505",
+      },
+      fontFamily: {
+        Inconsolata: ["Inconsolata", "monospace"],
+        Inter: ["Inter", "sans-serif"],
+        Lora: ["Lora", "serif"],
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
